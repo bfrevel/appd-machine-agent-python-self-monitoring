861689ee2cce17cd7ed3590ac9bffb03f79dcd8a
Bernd Frevel <bernd.frevel@appdynamics.com>
2022-05-13 13:20:09 +0200

    added nodes and backends

--- apis/application.py
+++ apis/application.py
@@ -1,3 +1,6 @@
+import asyncio
+
+
 class ApplicationApi:
     def __init__(self, config):
         self.config = config
@@ -16,10 +19,29 @@ class ApplicationApi:
             print(f"name={self.config['metric_path']}|Applications|{application['name']}|Business Transactions|Count, value={len(business_transactions)}")
             return business_transactions
 
-    async def tiers(self, session, application):
+    async def tiers_and_nodes(self, session, application):
         url = f"{self.config['controller_url']}/rest/applications/{application['id']}/tiers?output=json"
         async with session.get(url) as resp:
             tiers = await resp.json()
             print(f"name={self.config['metric_path']}|Applications|{application['name']}|Tiers|Count, value={len(tiers)}")
+            
+            node_tasks = []
+            for tier in tiers:
+                node_tasks.append(asyncio.ensure_future(self.nodes_by_tier(session, application, tier)))
+            await asyncio.gather(*node_tasks)
+
             return tiers
 
+    async def nodes_by_tier(self, session, application, tier):
+        url = f"{self.config['controller_url']}/rest/applications/{application['id']}/tiers/{tier['id']}/nodes?output=json"
+        async with session.get(url) as resp:
+            nodes = await resp.json()
+            print(f"name={self.config['metric_path']}|Applications|{application['name']}|Tiers|{tier['name']}|Nodes|Count, value={len(nodes)}")
+            return nodes
+
+    async def backends(self, session, application):
+        url = f"{self.config['controller_url']}/rest/applications/{application['id']}/backends?output=json"
+        async with session.get(url) as resp:
+            backends = await resp.json()
+            print(f"name={self.config['metric_path']}|Applications|{application['name']}|Backends|Count, value={len(backends)}")
+            return backends
--- self_monitoring.py
+++ self_monitoring.py
@@ -17,11 +17,18 @@ async def load_applications_information():
 
     async with aiohttp.ClientSession(headers = {'Authorization': f"Bearer {oauth_token}"}) as session:
         applications = await asyncio.ensure_future(applicationApi.applications(session))
-        application_tasks = []
+
+        business_transactions_tasks = []
+        tier_tasks = []
+        backend_tasks = []
         for application in applications:
-            application_tasks.append(asyncio.ensure_future(applicationApi.business_transactions(session, application)))
-            application_tasks.append(asyncio.ensure_future(applicationApi.tiers(session, application)))
-        await asyncio.gather(*application_tasks)
+            business_transactions_tasks.append(asyncio.ensure_future(applicationApi.business_transactions(session, application)))
+            tier_tasks.append(asyncio.ensure_future(applicationApi.tiers_and_nodes(session, application)))
+            backend_tasks.append(asyncio.ensure_future(applicationApi.backends(session, application)))
+        await asyncio.gather(*tier_tasks)
+        await asyncio.gather(*business_transactions_tasks)
+        await asyncio.gather(*backend_tasks)
+
 
 
 asyncio.run(load_applications_information())
